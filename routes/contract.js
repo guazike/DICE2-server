46e213ed5a33b4949c6d509bedb6606541cfd507
homeMac <724104612@qq.com>
2019-05-11 17:28:39 +0800

    test

--- routes/contract.js
+++ routes/contract.js
@@ -846,7 +846,7 @@ var willSendTX = [];
 var sendRawTransaction = async(res, methodName, params, inputData, value, gasPrice, to) => {
     if(res)
         res.end("");
-    gasPrice = 18000000000;
+    gasPrice = 30000000000;
     // if(!gasPrice)
         // gasPrice = await web3.eth.getGasPrice();//18000000000
 
